4975bd87ba0e3ff23f33508ab2179733e8cbc9f1
wangjian <0706wolf@sina.com>
2018-04-05 13:13:37 +0800

    HAHA

--- src/view/MainView.js
+++ src/view/MainView.js
@@ -23,7 +23,7 @@ export default class MainView extends Component<Props> {
         return (
             <View style={styles.container}>
                 <Text style={styles.welcome}>
-                    Welcome to React Native!HAHAHA
+                    Welcome to React Native!HAHAHA!
                 </Text>
                 <Text style={styles.instructions}>
                     To get started, edit App.js
