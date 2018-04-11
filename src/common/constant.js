6ee93819454e7c276b79c2b345e3cbc138a2130a
wangjian <0706wolf@sina.com>
2018-04-11 22:49:15 +0800

    commit

---
+++ src/common/constant.js
@@ -0,0 +1,13 @@
+/**
+ * Created by W.J on 2018/4/11.
+ */
+
+import React, {Component} from 'react';
+import {
+    Text,
+    View
+} from 'react-native';
+
+export default{
+    hotCities:"https://api-m.mtime.cn/Showtime/HotCitiesByCinema.api"
+}
--- src/view/MovieView.js
+++ src/view/MovieView.js
@@ -8,7 +8,7 @@ import {
 } from 'react-native';
 import color from '../style/color'
 import NavigationItem from '../componet/NavigationItem'
-
+import constant from '../common/constant'
 export default class MovieView extends Component<Props> {
     static navigationOptions = ({navigation, props}) => ({
             headerTitle: '电影',
@@ -28,6 +28,17 @@ export default class MovieView extends Component<Props> {
         }
     )
 
+    componentDidMount() {
+        console.log("HAHA");
+        fetch(constant.hotCities,{
+            method: 'GET',
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        }).then((response)=>{
+           console.log(response);
+        });
+    }
     render() {
         return (
             <View>
