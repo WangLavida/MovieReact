040636c61bb0957e9139be58b20bf78b117d0145
¹þ¹þ <59461@ddd4>
2018-04-17 18:14:21 +0800

    react-native-swiper

--- src/view/MovieView.js
+++ src/view/MovieView.js
@@ -4,12 +4,14 @@
 import React, {Component} from 'react';
 import {
     Text,
-    View
+    View,
+    Image
 } from 'react-native';
 import color from '../style/color'
 import NavigationItem from '../componet/NavigationItem'
 import constant from '../common/constant'
 import http from '../common/http'
+import Swiper from 'react-native-swiper';
 
 export default class MovieView extends Component<Props> {
     static navigationOptions = ({navigation, props}) => ({
@@ -30,20 +32,41 @@ export default class MovieView extends Component<Props> {
         }
     )
 
+    constructor(props) {
+        super();
+        this.state = {
+            swiperList: null
+        }
+    }
+
     componentDidMount() {
         var params = new Map();
         params.set("locationId", "295");
         http.post("即将上映", constant.comingNew, params, () => {
         }, (response) => {
+            this.setState({
+                swiperList: response.attention.slice(0, 6)
+            });
         }, (error) => {
 
         });
     }
 
+
     render() {
         return (
             <View>
-                <Text>MovieView</Text>
+                {
+                    this.state.swiperList != null ? <Swiper
+                        height={200} showsButtons={false} autoplay={true}>
+                        {
+                            this.state.swiperList.map((item) => {
+                                let uri = "uri:" + "'" + item.videos[0].image + "'";
+                                console.log(uri);
+                            })
+                        }
+                    </Swiper> : null
+                }
             </View>
         )
     }
